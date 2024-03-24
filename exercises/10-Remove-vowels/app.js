08039a2fdb1eff2870a43f12213ca9329cff5540
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-03-24 03:06:01 +0000

    All Exo Done

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,5 +1,14 @@
 // Your code goes here
-
+const rapid = (myString) => {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let newName = "";
+    for (let letters of myString.toLowerCase()) {
+        if (vowels.includes(letters) == false) {
+            newName += letters;
+        }
+    }
+    return newName.toUpperCase();
+}
 
 // Work above this line; do not change code below
 let str = "John";
